802f7a966485e530092440952648e569a9b52a5f
thawatkrit <thawatkritrubang@gmail.com>
2018-12-19 08:56:41 +0700

    initial file

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-    res.send(JSON.stringify(messages.getMessages()));
+    res.send(JSON.stringify(users.getUsers()));
 });
 
 app.post("/push", (req, res) => {
