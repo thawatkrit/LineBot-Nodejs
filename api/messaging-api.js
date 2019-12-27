f4b1f1feb584538a8d204a7db962a40e1dd6fb95
thawatkrit <thawatkritrubang@gmail.com>
2019-12-27 19:20:17 +0700

    hidden token

--- api/messaging-api.js
+++ api/messaging-api.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const accessToken = 'gAUGCPQSFxlvvlwwvO3EuUCQFJZR5cAf2hCBlZRrHJOXYlJYgEXS4Ba+xBr2VGmt4Kre3ID9eusD3DSx8JgMPJWR0uBrdUCh8FV6VIpDr+vSSYIKqcYhV/U3ujDyPv6LP+BQo61lH5Us2K+HIU2TFQdB04t89/1O/w1cDnyilFU=';
+const accessToken = '';
 
 function pushMessage (sender, text) {
     let data = {
